05a17a445d2639d7c4807a4bd077da6b7af9bdd8
Weiyi Shen <vicky18058128335@outlook.com>
2023-05-28 18:14:51 -0400

    hw4

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  /*function introductionWithLanguage(name, language) {
+    if (language != ''){
+        return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    } else {
+        return `Hi, my name is ${name} and I am learning to program in in JavaScript.`;
+    }
+}*/
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
